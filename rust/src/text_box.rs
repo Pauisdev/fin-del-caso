75ca3939899eb0ac9f3d2f4c335854c5521564f1
pauisdev <ezepuntocom@hotmail.com>
2026-06-25 02:40:01 -0300

    Found cause of bugs

--- rust/src/text_box.rs
+++ rust/src/text_box.rs
@@ -7,10 +7,11 @@ use godot::{classes::ITextureRect, register::GodotClass};
 #[class(init, base=TextureRect)]
 pub struct TextBox {
     #[export]
-    #[init(val = 20)]
-    text_speed: i32,
+    #[init(val = 20.0)]
+    text_speed: f64,
     text_to_show: Option<String>,
-    ignored_first_click: bool,
+    characters_to_show: f64,
+    awaiting_release: bool,
     base: Base<TextureRect>,
 }
 
@@ -18,26 +19,30 @@ pub struct TextBox {
 impl ITextureRect for TextBox {
     fn ready(&mut self) {
         self.base_mut().set_visible(false);
-        godot_print!("Puesto invisible");
     }
 
     fn process(&mut self, delta: f64) {
-        godot_print!("HOLA");
         let Some(text_to_show) = self.text_to_show.as_ref() else {
-            godot_print!("Falló");
             return;
         };
-        godot_print!("Aqui el delta: {delta}");
+        self.characters_to_show += delta * self.text_speed;
         let mut label = self.base().get_node_as::<RichTextLabel>("Text");
-        label.set_visible_characters(delta as i32 * self.text_speed);
+        if label.get_visible_characters() != -1 {
+            label.set_visible_characters(self.characters_to_show as i32);
+        }
 
         let input = Input::singleton();
+
+        if input.is_action_just_released("click") {
+            self.awaiting_release = false;
+        }
+
         if input.is_action_just_pressed("click") {
-            if !self.ignored_first_click {
-                self.ignored_first_click = true;
+            if self.awaiting_release {
                 return;
             }
             if label.get_visible_characters() < text_to_show.len() as i32 {
+                // TODO: this wont work anymore since im now setting it to –1
                 label.set_visible_characters(-1);
                 return;
             }
@@ -54,10 +59,11 @@ impl TextBox {
             return;
         }
         let mut label = self.base().get_node_as::<RichTextLabel>("Text");
-        godot_print!("Aparecer textito");
         label.set_visible_characters(0);
         label.set_text(&text);
-        self.ignored_first_click = false;
+        self.text_to_show = Some(text);
+        self.characters_to_show = 0.0;
+        self.awaiting_release = true;
         self.base_mut().set_visible(true);
     }
 }
